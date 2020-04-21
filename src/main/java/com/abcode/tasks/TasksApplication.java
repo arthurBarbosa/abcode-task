55edd67eccf8742aae40ac46d0e3b8c87a515faf
Arthur Barbosa <arthurbruno03@gmail.com>
2020-04-21 12:35:02 -0400

    Expondo o ID das tarefas via web services

--- src/main/java/com/abcode/tasks/TasksApplication.java
+++ src/main/java/com/abcode/tasks/TasksApplication.java
@@ -1,13 +1,20 @@
 package com.abcode.tasks;
 
+import com.abcode.tasks.domain.task.Task;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 
 @SpringBootApplication
-public class TasksApplication {
+public class TasksApplication implements RepositoryRestConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
 	}
 
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		config.exposeIdsFor(Task.class);
+	}
 }
