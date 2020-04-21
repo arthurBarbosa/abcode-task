d52ee5a564d7bc43fbf9bfed4bfea15da51e95be
Arthur Barbosa <arthurbruno03@gmail.com>
2020-04-21 13:07:23 -0400

    Configurando o processo de validação para retornar os erros corretamente

--- src/main/java/com/abcode/tasks/TasksApplication.java
+++ src/main/java/com/abcode/tasks/TasksApplication.java
@@ -3,18 +3,35 @@ package com.abcode.tasks;
 import com.abcode.tasks.domain.task.Task;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
 
 @SpringBootApplication
 public class TasksApplication implements RepositoryRestConfigurer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TasksApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TasksApplication.class, args);
+    }
 
-	@Override
-	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-		config.exposeIdsFor(Task.class);
-	}
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Task.class);
+    }
+
+    @Bean
+    public Validator validator() {
+        return new LocalValidatorFactoryBean();
+    }
+
+    @Override
+    public void configureValidatingRepositoryEventListener(ValidatingRepositoryEventListener validatingListener) {
+        Validator validator = validator();
+        validatingListener.addValidator("beforeCreate", validator);
+        validatingListener.addValidator("beforeCreate", validator);
+    }
 }
